ab59f464839612c784fb85e6dba0a667450f7036
iRatkin <ADH.2234@gmail.com>
2023-12-27 01:37:46 +0300

    Finished home page

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,8 +1,17 @@
 import React, { PropTypes } from 'react';
-import Input from '../../components/ui/input/index';
 import { bindAll } from 'lodash';
 import { connect } from 'react-redux';
-import { addTodo } from './actions';
+import { 
+    addTodo, 
+    likeTodo, 
+    deleteTodo, 
+    getTodos,
+    saveTodos
+} from './actions';
+import Input from '../../components/ui/input/index';
+import Loader from '../../components/ui/loader/index';
+import classnames from 'classnames';
+import { LS } from '../../utils/index';
 import './styles.less';
 
 class HomePage extends React.Component {
@@ -22,6 +31,10 @@ class HomePage extends React.Component {
 
         bindAll(this, ['renderTodos', 'inputOnChange', 'addTodo']);
     }
+    
+    componentWillMount() {
+        this.props.dispatch( getTodos() );
+    }
 
     inputOnChange(value) {
         this.setState({ todoName: value });
@@ -36,19 +49,41 @@ class HomePage extends React.Component {
     }
 
     renderTodos(item, idx) {
+        const todoClasses = classnames('b-home-todo', {
+            'is-liked': item.liked
+        });
+        const btnClasses = classnames('btn', {
+            'active': item.liked
+        });
         return (
-            <li key={ idx }>{ item.name }</li>
+            <li key={ idx }>
+                <span className={ todoClasses }>{ item.name }</span> 
+                <button className='btn' onClick={ this.deleteTodo.bind(this, item) }><i className='glyphicon glyphicon-remove' /></button>
+                <button className={ btnClasses } onClick={ this.likeTodo.bind(this, item) }><i className='glyphicon glyphicon-heart' /></button>
+            </li>
         );
+    }
+
+    deleteTodo(todo) {
+        this.props.dispatch( deleteTodo(todo) );
+    }
+
+    likeTodo(todo) {
+        this.props.dispatch( likeTodo(todo) );
     }
     
     render() {
         const { todoName } = this.state;
         const { todos, error } = this.props.home;
+        LS.set('todos', todos);
         return (
             <div className='row-fluid b-home'>
                 <div className='col-xs-12'>
                     <ul>
-                        { todos.map(this.renderTodos) }
+                        {
+                            todos.length === 0 ? <Loader /> :
+                            todos.map(this.renderTodos) 
+                        }
                     </ul>
                     <div className='col-xs-4'>
                         <Input
@@ -56,12 +91,16 @@ class HomePage extends React.Component {
                             value={ todoName }
                             error={ error }
                         />
-                        <button className='btn btn-primary' onClick={ this.addTodo }>Add todo</button>
+                        <button className='btn btn-primary b-home-submit' onClick={ this.addTodo }>Add todo</button>
                     </div>
                 </div>
             </div>
         );
     }
+
+    // componentWillUnmount() {
+    //
+    // }
     
 }
 
